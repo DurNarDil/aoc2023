9b105f6199f48a65a0deefdfbfdab1de15f3fdc0
DURNARDIL\Durnardil <durnardil@gmail.com>
2023-12-03 21:09:47 +0100

    Test case

--- run.py
+++ run.py
@@ -2,10 +2,10 @@ from variables import *
 
 lineList = input.split("\n")
 digitSum = 0
-for line in lineList:
-    digits = concat_first_last_digit(line)
-    # digits = concat_first_last_digit_or_spelled_out(line)
-    digitSum = digitSum + int(digits)
+for index,line in enumerate(lineList):
+    value = game_info(line)
+    if value:
+      digitSum = digitSum + index +1
     
 print(digitSum)
                 
